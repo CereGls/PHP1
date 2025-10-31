cd96087d036c41a23f05fe5d70fad97ae66d7f9c
Cereda Giovanni 2 <cereda2.giovanni@osdbsesto>
2025-10-31 14:33:46 +0100

    save

--- calcolatricePRO/index.php
+++ calcolatricePRO/index.php
@@ -1,3 +1,14 @@
+<?php
+$input;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST['hide'] == "") {
+        $input = $_POST['num1'] . $_POST['oper'];
+    } else {
+        $input = $_POST['hide'] . $_POST['num1'] . $_POST['oper'];
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,11 +23,15 @@
     <h3>Inserisci i due numeri e seleziona l'operazione da svolgere</h3>
     
     <form action="index.php" method="post">
-        <input type="number" name="num1" id="num1" value="<?php echo $_POST['num1'] ?? '0'; ?>" required>
-        <button type="button" id="btn" name="oper" Value="somma">+</button>
-        <button type="button" id="btn" name="oper" value="sottra">-</button>
-        <button type="button" id="btn" name="oper" value="molti">*</button>
-        <button type="button" id="btn" name="oper" value="divi">/</button>
+        <input type="number" name="num1" id="num1" value="<?php echo $_POST['num1']; ?>" required>
+            <label for="showexp"><?php echo $input ?? ""; ?></label>
+            <?php echo $_POST['num1']; ?>
+        </input>
+        <input type="text" name="hide" id="hide" value="<?php echo $input; ?>" hidden>
+        <button type="submit" id="summ" name="oper" Value="+">+</button>
+        <button type="submit" id="sottr" name="oper" value="-">-</button>
+        <button type="submit" id="moltip" name="oper" value="*">*</button>
+        <button type="submit" id="divi" name="oper" value="/">/</button>
         <input type="submit" value="=">
     </form>
 </body>
